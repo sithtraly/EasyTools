55413502380be3b1fa0e899c21741507b8a9b82f
Sith Traly <sithtraly147@gmail.com>
2026-02-16 13:34:01 +0700

    Update sis2geip to order name follow name copy from geip

--- sis2geip/sis2geip.js
+++ sis2geip/sis2geip.js
@@ -7,82 +7,20 @@ createApp({
 	setup() {
 		const files = ref([])
 		const isDragOver = ref(false)
-		const data = ref([])
 		const error = ref('')
+		const step = ref(1)
+		const wb = ref()
+		const tabs = ref([])
+		const activeTab = ref()
+		const studentsForSort = ref({})
+		const isLoading = ref(false)
+
+		const dataSheets = ref({})
 
 		const allowedTypes = [
 			"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
 			"application/vnd.ms-excel"
 		]
-
-		// const geipScoreHeaders = [
-		// 	{ no: 1, subject: "សរសេរតាមអាន", },
-		// 	{ no: 2, subject: "តែងសេចក្តី", },
-		// 	{ no: 3, subject: "ល្បឿនអំណាន", },
-		// 	{ no: 4, subject: "ភាសាខ្មែរ", },
-		// 	{ no: 5, subject: "គណិតវិទ្យា", },
-		// 	{ no: 6, subject: "រូបវិទ្យា", },
-		// 	{ no: 7, subject: "គីមីវិទ្យា", },
-		// 	{ no: 8, subject: "ជីវវិទ្យា", },
-		// 	{ no: 9, subject: "ប្រវត្តិវិទ្យា", },
-		// 	{ no: 10, subject: "ព័ត៌មានវិទ្យា", },
-		// 	{ no: 11, subject: "សីល-ពលរដ្ឋ", },
-		// 	{ no: 12, subject: "ផែនដីវិទ្យា", },
-		// 	{ no: 13, subject: "ភូមិវិទ្យា", },
-		// 	{ no: 14, subject: "គេហវិទ្យា", },
-		// 	{ no: 15, subject: "អប់រំកាយ", },
-		// 	{ no: 16, subject: "ភាសាចិន/បំណិន", },
-		// 	{ no: 17, subject: "សេដ្ឋកិច្ច", },
-		// 	{ no: 18, subject: "សិល្បៈ", },
-		// 	{ no: 19, subject: "កសិកម្ម", },
-		// 	{ no: 20, subject: "ភាសាបរទេស", },
-		// ]
-
-		// const sisSubjectHeaders = [
-		// 	{ no: 1, subject: "សរសេរតាមអាន", },
-		// 	{ no: 2, subject: "តែងសេចក្តី", },
-		// 	{ no: 3, subject: "", },
-		// 	{ no: 4, subject: "អក្សរសាស្រ្តខ្មែរ", },
-		// 	{ no: 5, subject: "គណិតវិទ្យា", },
-		// 	{ no: 6, subject: "រូបវិទ្យា", },
-		// 	{ no: 7, subject: "គីមីវិទ្យា", },
-		// 	{ no: 8, subject: "ជីវវិទ្យា", },
-		// 	{ no: 9, subject: "ប្រវត្តិវិទ្យា", },
-		// 	{ no: 10, subject: "បច្ចេកវិទ្យាគមនាគមន៍និងព័ត៏មាន", },
-		// 	{ no: 11, subject: "សីលធម៌-ពលរដ្ឋវិជ្ជា", },
-		// 	{ no: 12, subject: "ផែនដីវិទ្យា", },
-		// 	{ no: 13, subject: "ភូមិវិទ្យា", },
-		// 	{ no: 14, subject: "គេហវិទ្យា", },
-		// 	{ no: 15, subject: "អប់រំកាយ-កីឡា", },
-		// 	{ no: 16, subject: "បំណិនជីវិត", },
-		// 	{ no: 17, subject: "សេដ្ឋកិច្ច", },
-		// 	{ no: 18, subject: "អប់រំសុខភាព", },
-		// 	{ no: 19, subject: "កសិកម្ម", },
-		// 	{ no: 20, subject: "អង់គ្លេស", },
-		// ]
-
-		// const subjectHeaders = [
-		// 	{ no: 1, sis: "សរសេរតាមអាន", geip: "សរសេរតាមអាន", },
-		// 	{ no: 2, sis: "តែងសេចក្តី", geip: "តែងសេចក្តី", },
-		// 	{ no: 3, sis: "", geip: "ល្បឿនអំណាន", },
-		// 	{ no: 4, sis: "អក្សរសាស្រ្តខ្មែរ", geip: "ភាសាខ្មែរ", },
-		// 	{ no: 5, sis: "គណិតវិទ្យា", geip: "គណិតវិទ្យា", },
-		// 	{ no: 6, sis: "រូបវិទ្យា", geip: "រូបវិទ្យា", },
-		// 	{ no: 7, sis: "គីមីវិទ្យា", geip: "គីមីវិទ្យា", },
-		// 	{ no: 8, sis: "ជីវវិទ្យា", geip: "ជីវវិទ្យា", },
-		// 	{ no: 9, sis: "ប្រវត្តិវិទ្យា", geip: "ប្រវត្តិវិទ្យា", },
-		// 	{ no: 10, sis: "បច្ចេកវិទ្យាគមនាគមន៍និងព័ត៏មាន", geip: "ព័ត៌មានវិទ្យា", },
-		// 	{ no: 11, sis: "សីលធម៌-ពលរដ្ឋវិជ្ជា", geip: "សីល-ពលរដ្ឋ", },
-		// 	{ no: 12, sis: "ផែនដីវិទ្យា", geip: "ផែនដីវិទ្យា", },
-		// 	{ no: 13, sis: "ភូមិវិទ្យា", geip: "ភូមិវិទ្យា", },
-		// 	{ no: 14, sis: "គេហវិទ្យា", geip: "គេហវិទ្យា", },
-		// 	{ no: 15, sis: "អប់រំកាយ-កីឡា", geip: "អប់រំកាយ", },
-		// 	{ no: 16, sis: "បំណិនជីវិត", geip: "ភាសាចិន/បំណិន", },
-		// 	{ no: 17, sis: "សេដ្ឋកិច្ច", geip: "សេដ្ឋកិច្ច", },
-		// 	{ no: 18, sis: "អប់រំសុខភាព", geip: "សិល្បៈ", },
-		// 	{ no: 19, sis: "កសិកម្ម", geip: "កសិកម្ម", },
-		// 	{ no: 20, sis: "អង់គ្លេស", geip: "ភាសាបរទេស", },
-		// ]
 
 		const geipScoreHeaders = ["សរសេរតាមអាន", "តែងសេចក្តី", "ល្បឿនអំណាន", "ភាសាខ្មែរ", "គណិតវិទ្យា", "រូបវិទ្យា", "គីមីវិទ្យា", "ជីវវិទ្យា", "ប្រវត្តិវិទ្យា", "ព័ត៌មានវិទ្យា", "សីល-ពលរដ្ឋ", "ផែនដីវិទ្យា", "ភូមិវិទ្យា", "គេហវិទ្យា", "អប់រំកាយ", "ភាសាចិន/បំណិន", "សេដ្ឋកិច្ច", "សិល្បៈ", "កសិកម្ម", "ភាសាបរទេស",]
 		const sisSubjectHeaders = ["សរសេរតាមអាន", "តែងសេចក្តី", "", "អក្សរសាស្រ្តខ្មែរ", "គណិតវិទ្យា", "រូបវិទ្យា", "គីមីវិទ្យា", "ជីវវិទ្យា", "ប្រវត្តិវិទ្យា", "បច្ចេកវិទ្យាគមនាគមន៍និងព័ត៏មាន", "សីលធម៌-ពលរដ្ឋវិជ្ជា", "ផែនដីវិទ្យា", "ភូមិវិទ្យា", "គេហវិទ្យា", "អប់រំកាយ-កីឡា", "បំណិនជីវិត", "សេដ្ឋកិច្ច", "អប់រំសុខភាព", "កសិកម្ម", "អង់គ្លេស",]
@@ -91,13 +29,13 @@ createApp({
 			const reader = new FileReader()
 
 			reader.onload = async (event) => {
-				let buffer = event.target.result
-				const wb = new ExcelJS.Workbook()
-				await wb.xlsx.load(buffer)
+				const buffer = event.target.result
+				wb.value = new ExcelJS.Workbook()
+				await wb.value.xlsx.load(buffer)
 
-				const wslength = wb.worksheets.length
+				const wslength = wb.value.worksheets.length
 				for (let i = 0; i < wslength; i++) {
-					const ws = wb.worksheets[i]
+					const ws = wb.value.worksheets[i]
 					let rows = []
 					rows[0] = ws.getRow(9).values.map(v => v ? v.replaceAll(' ', '') : v)
 					ws.eachRow({ includeEmpty: true }, (row, i) => {
@@ -155,21 +93,51 @@ createApp({
 						return row
 					})
 
-					const newWs = wb.addWorksheet(`New ${ws.name}`)
-					newWs.insertRows(1, rows)
+					// const newWs = wb.addWorksheet(`New ${ws.name}`)
+					// newWs.insertRows(1, rows)
+					dataSheets.value[ws.name] = rows
 				}
-
-				// Download file
-				buffer = await wb.xlsx.writeBuffer()
-				const blob = new Blob([buffer], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" })
-				const link = document.createElement('a')
-				link.download = `Prepared-${file.name}`
-				link.href = URL.createObjectURL(blob)
-				link.click()
-				URL.revokeObjectURL(link.href)
 			}
 
 			reader.readAsArrayBuffer(file)
+		}
+
+		const sortByProvidedName = () => {
+			for (const col of tabs.value) {
+				const [header, ...data] = dataSheets.value[col]
+				const originOrder = studentsForSort.value[col]
+
+				if (originOrder) {
+					const orderMap = new Map()
+					originOrder.split('\n').forEach((name, index) => orderMap.set(name, index))
+					data.sort((a, b) => {
+						const ia = orderMap.has(a[2]) ? orderMap.get(a[2]) : Infinity
+						const ib = orderMap.has(b[2]) ? orderMap.get(b[2]) : Infinity
+						return ia - ib
+					})
+				}
+
+				const ws = wb.value.addWorksheet(`prepared-${col}`)
+				ws.insertRows(1, [header, ...data])
+			}
+		}
+
+		const downLoadFile = async () => {
+			isLoading.value = true
+			setTimeout(async () => {
+				sortByProvidedName()
+				// Download file
+				const buffer = await wb.value.xlsx.writeBuffer()
+				const blob = new Blob([buffer], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" })
+				const link = document.createElement('a')
+				link.download = `Prepared-${files.value[0].name}`
+				link.href = URL.createObjectURL(blob)
+				link.click()
+				URL.revokeObjectURL(link.href)
+				step.value = 1
+				isLoading.value = false
+				files.value = []
+			}, 2)
 		}
 
 		const validateFile = (file) => {
@@ -181,13 +149,13 @@ createApp({
 			return true
 		}
 
-		const handleDrop = (event) => {
+		const handleDrop = async (event) => {
 			event.preventDefault()
 			isDragOver.value = false
 			const _files = [...event.dataTransfer.files]
 			const validated = validateFile(_files[0])
-			readExcel(_files[0])
 			files.value = validated ? _files : []
+			wb.value = await readExcel(_files[0])
 		}
 
 		const handleDragOver = (event) => {
@@ -199,26 +167,40 @@ createApp({
 			isDragOver.value = false
 		}
 
-		const hanldeFileSelect = (event) => {
+		const hanldeFileSelect = async (event) => {
 			files.value = [...event.target.files]
-			readExcel(files.value[0])
+			wb.value = await readExcel(files.value[0])
+		}
+
+		const next = async () => {
+			step.value++
+			tabs.value = Object.keys(dataSheets.value)
+			activeTab.value = tabs.value[0]
 		}
 
 		return {
 			files,
 			isDragOver,
 			error,
+			step,
+			dataSheets,
+			tabs,
+			activeTab,
+			studentsForSort,
+			isLoading,
 			handleDrop,
 			handleDragOver,
 			handleDragLeave,
 			hanldeFileSelect,
 			validateFile,
+			next,
+			downLoadFile,
 		}
 	},
 
 	template: `
-		<div>
-			<h2>Drag & Drop File Upload</h2>
+		<div  v-if="step == 1">
+			<h2>បញ្ចូលឯកសារ</h2>
 
 			<label
 				class="drop-zone"
@@ -235,8 +217,8 @@ createApp({
 					accept=".xlsx, .xlsn, .xlsb, .xls, .xls, .xlsx, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 				/>
 
-				<p>Drag files here or click to upload</p>
-				<p>Support file extensions: .xlsx, .xlsn, .xlsb, .xls</p>
+				<p>ទាញ ឬចុចដើម្បីបញ្ចូល</p>
+				<p>ឯកសារដែលអាចប្រើបាន: .xlsx, .xlsn, .xlsb, .xls</p>
 			</label>
 
 			<div class="file-list" v-if="files.length">
@@ -249,8 +231,24 @@ createApp({
 			</div>
 
 			<div class="error" v-if="error">\
-				<h4>Unsuported file type</h4>
+				<h4>ឯកសារមិនគាំទ្រ</h4>
 				<p>{{error}}</p>
+			</div>
+			<button @click="next()" v-if="files.length" class="bt-primary float-right">បន្ទាប់ &rArr;</button>
+		</div>
+
+		<div v-if="step == 2">
+			<p>បញ្ចូលឈ្មោះដែលត្រូវតម្រៀបតាម</p>
+			<div class="tabs">
+				<button v-for="tab in tabs" class="tab" :class="{'tab-active': tab == activeTab}" @click="activeTab = tab">{{tab}}</button>
+				<textarea v-for="tab in tabs" :id="tab" v-model="studentsForSort[tab]" :key="tab" style="width: 100%" rows="20" v-show="tab == activeTab">{{studentsForSort[tab]}}</textarea>
+				<button @click="downLoadFile()" class="bt-primary float-right" :disabled="isLoading">
+					<span v-if="!isLoading">ទាញយក &darr;</span>
+					<div style="display: flex;align-items: center;" v-if="isLoading" >
+						<span margin-right: 10px>Downloading</span>
+						<div class="loader"></div>
+					</div>
+				</button>
 			</div>
 		</div>
 	`
