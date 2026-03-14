486b23f96617102997d18172bf425f4650fbdf90
Sith Traly <sithtraly147@gmail.com>
2026-03-14 08:30:51 +0700

    fix error asset and link error firebase

--- src/helpers/link.helper.ts
+++ src/helpers/link.helper.ts
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-	if (location.href.includes('localhost')) {
+	if (!location.href.includes('sithtraly.github.io')) {
 		const links = document.querySelectorAll('a')
 		links.forEach(link => {
 			link.href = link.href.replace('/EasyTools/', '/')
